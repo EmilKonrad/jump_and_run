faf244fd39b7ae82a504d45cec2f297699c2a592
EmilKonrad <lime.konrad@gmail.com>
2025-02-20 14:52:17 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,391 @@
+function Erstelle_Charakter2 () {
+    RBTFT18.drawLine(
+    x_position_charakter + 1,
+    y_position_charakter,
+    x_position_charakter + 5,
+    y_position_charakter,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 2,
+    y_position_charakter - 1,
+    x_position_charakter + 6,
+    y_position_charakter - 1,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter + 4,
+    y_position_charakter - 2,
+    x_position_charakter + 6,
+    y_position_charakter - 2,
+    Color.Black
+    )
+    RBTFT18.drawLine(
+    x_position_charakter,
+    y_position_charakter - 2,
+    x_position_charakter + 3,
+    y_position_charakter - 2,
+    Color.Yellow
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 2, Color.Black)
+    RBTFT18.drawLine(
+    x_position_charakter - 2,
+    y_position_charakter - 3,
+    x_position_charakter + 7,
+    y_position_charakter - 3,
+    Color.Yellow
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 3, Color.Black)
+    RBTFT18.drawPixel(x_position_charakter + 5, y_position_charakter - 3, Color.Black)
+    RBTFT18.drawPixel(x_position_charakter + 7, y_position_charakter - 3, Color.Black)
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 4,
+    x_position_charakter + 7,
+    y_position_charakter - 4,
+    Color.Yellow
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 4, Color.Black)
+    RBTFT18.drawPixel(x_position_charakter, y_position_charakter - 4, Color.Black)
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 4, Color.Black)
+    RBTFT18.drawPixel(x_position_charakter + 5, y_position_charakter - 4, Color.Black)
+    RBTFT18.drawLine(
+    x_position_charakter - 2,
+    y_position_charakter - 5,
+    x_position_charakter + 7,
+    y_position_charakter - 5,
+    Color.Black
+    )
+    RBTFT18.drawLine(
+    x_position_charakter,
+    y_position_charakter - 5,
+    x_position_charakter + 3,
+    y_position_charakter - 5,
+    Color.Yellow
+    )
+    RBTFT18.drawLine(
+    x_position_charakter,
+    y_position_charakter - 6,
+    x_position_charakter + 6,
+    y_position_charakter - 6,
+    Color.Yellow
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 7,
+    x_position_charakter + 4,
+    y_position_charakter - 7,
+    Color.Blue
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 7, Color.Red)
+    RBTFT18.drawLine(
+    x_position_charakter - 2,
+    y_position_charakter - 8,
+    x_position_charakter + 7,
+    y_position_charakter - 8,
+    Color.Blue
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 8, Color.Red)
+    RBTFT18.drawPixel(x_position_charakter + 4, y_position_charakter - 8, Color.Red)
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 9,
+    x_position_charakter + 8,
+    y_position_charakter - 9,
+    Color.Blue
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 9, Color.Red)
+    RBTFT18.drawPixel(x_position_charakter + 4, y_position_charakter - 9, Color.Red)
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 10,
+    x_position_charakter + 8,
+    y_position_charakter - 10,
+    Color.Yellow
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 10,
+    x_position_charakter + 6,
+    y_position_charakter - 10,
+    Color.Blue
+    )
+    RBTFT18.drawLine(
+    x_position_charakter + 1,
+    y_position_charakter - 10,
+    x_position_charakter + 4,
+    y_position_charakter - 10,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 11,
+    x_position_charakter + 8,
+    y_position_charakter - 11,
+    Color.Yellow
+    )
+    RBTFT18.drawLine(
+    x_position_charakter,
+    y_position_charakter - 11,
+    x_position_charakter + 5,
+    y_position_charakter - 11,
+    Color.Red
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 11, Color.Yellow)
+    RBTFT18.drawPixel(x_position_charakter + 4, y_position_charakter - 11, Color.Yellow)
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 12,
+    x_position_charakter + 8,
+    y_position_charakter - 12,
+    Color.Yellow
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 12,
+    x_position_charakter + 6,
+    y_position_charakter - 12,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 13,
+    x_position_charakter + 1,
+    y_position_charakter - 13,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter + 4,
+    y_position_charakter - 13,
+    x_position_charakter + 6,
+    y_position_charakter - 13,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 2,
+    y_position_charakter - 14,
+    x_position_charakter,
+    y_position_charakter - 14,
+    Color.Maroon
+    )
+    RBTFT18.drawLine(
+    x_position_charakter + 5,
+    y_position_charakter - 14,
+    x_position_charakter + 7,
+    y_position_charakter - 14,
+    Color.Maroon
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 15,
+    x_position_charakter,
+    y_position_charakter - 15,
+    Color.Maroon
+    )
+    RBTFT18.drawLine(
+    x_position_charakter + 5,
+    y_position_charakter - 15,
+    x_position_charakter + 8,
+    y_position_charakter - 15,
+    Color.Maroon
+    )
+}
+function Erstelle_Charakter () {
+    RBTFT18.drawLine(
+    x_position_charakter,
+    y_position_charakter,
+    x_position_charakter + 4,
+    y_position_charakter,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 1,
+    x_position_charakter + 7,
+    y_position_charakter - 1,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 2,
+    x_position_charakter + 1,
+    y_position_charakter - 2,
+    Color.Black
+    )
+    RBTFT18.drawLine(
+    x_position_charakter + 2,
+    y_position_charakter - 2,
+    x_position_charakter + 5,
+    y_position_charakter - 2,
+    Color.Red
+    )
+    RBTFT18.drawPixel(x_position_charakter + 4, y_position_charakter - 2, Color.Black)
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 3,
+    x_position_charakter + 7,
+    y_position_charakter - 3,
+    Color.Red
+    )
+    RBTFT18.drawPixel(x_position_charakter - 2, y_position_charakter - 3, Color.Black)
+    RBTFT18.drawPixel(x_position_charakter + 0, y_position_charakter - 3, Color.Black)
+    RBTFT18.drawPixel(x_position_charakter + 4, y_position_charakter - 3, Color.Black)
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 4,
+    x_position_charakter + 8,
+    y_position_charakter - 4,
+    Color.Yellow
+    )
+    RBTFT18.drawPixel(x_position_charakter - 2, y_position_charakter - 4, Color.Black)
+    RBTFT18.drawPixel(x_position_charakter, y_position_charakter - 4, Color.Black)
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 4, Color.Black)
+    RBTFT18.drawPixel(x_position_charakter + 5, y_position_charakter - 4, Color.Black)
+    RBTFT18.drawLine(
+    x_position_charakter - 2,
+    y_position_charakter - 5,
+    x_position_charakter + 7,
+    y_position_charakter - 5,
+    Color.Black
+    )
+    RBTFT18.drawLine(
+    x_position_charakter,
+    y_position_charakter - 5,
+    x_position_charakter + 3,
+    y_position_charakter - 5,
+    Color.Yellow
+    )
+    RBTFT18.drawLine(
+    x_position_charakter,
+    y_position_charakter - 6,
+    x_position_charakter + 6,
+    y_position_charakter - 6,
+    Color.Yellow
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 7,
+    x_position_charakter + 4,
+    y_position_charakter - 7,
+    Color.Blue
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 7, Color.Red)
+    RBTFT18.drawLine(
+    x_position_charakter - 2,
+    y_position_charakter - 8,
+    x_position_charakter + 7,
+    y_position_charakter - 8,
+    Color.Blue
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 8, Color.Red)
+    RBTFT18.drawPixel(x_position_charakter + 4, y_position_charakter - 8, Color.Red)
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 9,
+    x_position_charakter + 8,
+    y_position_charakter - 9,
+    Color.Blue
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 9, Color.Red)
+    RBTFT18.drawPixel(x_position_charakter + 4, y_position_charakter - 9, Color.Red)
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 10,
+    x_position_charakter + 8,
+    y_position_charakter - 10,
+    Color.Yellow
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 10,
+    x_position_charakter + 6,
+    y_position_charakter - 10,
+    Color.Blue
+    )
+    RBTFT18.drawLine(
+    x_position_charakter + 1,
+    y_position_charakter - 10,
+    x_position_charakter + 4,
+    y_position_charakter - 10,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 11,
+    x_position_charakter + 8,
+    y_position_charakter - 11,
+    Color.Yellow
+    )
+    RBTFT18.drawLine(
+    x_position_charakter,
+    y_position_charakter - 11,
+    x_position_charakter + 5,
+    y_position_charakter - 11,
+    Color.Red
+    )
+    RBTFT18.drawPixel(x_position_charakter + 1, y_position_charakter - 11, Color.Yellow)
+    RBTFT18.drawPixel(x_position_charakter + 4, y_position_charakter - 11, Color.Yellow)
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 12,
+    x_position_charakter + 8,
+    y_position_charakter - 12,
+    Color.Yellow
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 12,
+    x_position_charakter + 6,
+    y_position_charakter - 12,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 1,
+    y_position_charakter - 13,
+    x_position_charakter + 1,
+    y_position_charakter - 13,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter + 4,
+    y_position_charakter - 13,
+    x_position_charakter + 6,
+    y_position_charakter - 13,
+    Color.Red
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 2,
+    y_position_charakter - 14,
+    x_position_charakter,
+    y_position_charakter - 14,
+    Color.Maroon
+    )
+    RBTFT18.drawLine(
+    x_position_charakter + 5,
+    y_position_charakter - 14,
+    x_position_charakter + 7,
+    y_position_charakter - 14,
+    Color.Maroon
+    )
+    RBTFT18.drawLine(
+    x_position_charakter - 3,
+    y_position_charakter - 15,
+    x_position_charakter,
+    y_position_charakter - 15,
+    Color.Maroon
+    )
+    RBTFT18.drawLine(
+    x_position_charakter + 5,
+    y_position_charakter - 15,
+    x_position_charakter + 8,
+    y_position_charakter - 15,
+    Color.Maroon
+    )
+}
+let x_position_charakter = 0
+let y_position_charakter = 0
+y_position_charakter = 50
+x_position_charakter = 50
+basic.forever(function () {
+    Erstelle_Charakter()
+})
